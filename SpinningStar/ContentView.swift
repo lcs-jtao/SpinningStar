d63ec9a27ac4df7895576cb43718a7ea5c84c70d
Joyce Tao <jtao@lcs.on.ca>
2022-04-27 10:08:09 -0400

    Star moves from left to right with spins

--- SpinningStar/ContentView.swift
+++ SpinningStar/ContentView.swift
@@ -11,8 +11,11 @@ struct ContentView: View {
     
     // MARK: Stored properties
     
-    // Starting position for the star (initial state)
+    // 1. Starting position for the star (initial state)
     @State var xOffset = -100.0
+    
+    // Control rotation of the star
+    @State var starRotation = 0.0
     
     // MARK: Computed properties
     var body: some View {
@@ -20,7 +23,8 @@ struct ContentView: View {
             .resizable()
             .frame(width: 40, height: 40)
             .foregroundColor(.blue)
-            .offset(x: xOffset, y: 0)
+            .rotation3DEffect(Angle.degrees(starRotation), axis: (x: 0.0, y: 0.0, z: 1.0))
+            .offset(x: xOffset, y: 0.0)
             .animation(
                 Animation
                     .easeInOut(duration: 2)
@@ -30,6 +34,8 @@ struct ContentView: View {
             .onTapGesture {
                 // 2. Logic that changes the state
                 xOffset = 100.0
+                // Spin the star twice
+                starRotation = 360 * 2
             }
     }
 }
